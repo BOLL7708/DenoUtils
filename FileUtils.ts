75e25253c20061aa4e78838bbfc6e6cc79921ce9
Andreas Aronsson <BOLL@7708.net>
2025-07-19 19:37:27 +0200

    Expand FileUtils, add error handling

--- FileUtils.ts
+++ FileUtils.ts
@@ -1,13 +1,84 @@
+import * as path from 'jsr:@std/path'
+
 export default class FileUtils {
-    static loadTextFile(path: string): string|undefined {
-        let bytes: Uint8Array = new Uint8Array(0)
+    static ensureDir(anyPath: string): boolean {
+        const dirPath = path.dirname(anyPath)
         try {
-            bytes = Deno.readFileSync(path)
-        } catch(e) {
-            console.error(e)
+            Deno.mkdirSync(dirPath, {recursive: true})
+        } catch (e) {
+            if (e instanceof Deno.errors.AlreadyExists) return true
+            if (e instanceof Deno.errors.PermissionDenied) return false
+            console.error('Unhandled error when attempting to create directory', e)
+            return false
+        }
+        return true
+    }
+
+    /** Will attempt to load a text file from disk, returns undefined if failed. */
+    static readText(filepath: string): string | undefined {
+        let text = ''
+        try {
+            text = Deno.readTextFileSync(filepath)
+        } catch (e) {
+            if (e instanceof Deno.errors.NotFound) return
+            if (e instanceof Deno.errors.PermissionDenied) return
+            if (e instanceof Deno.errors.IsADirectory) return
+            console.error('Unhandled error when attempting to read file', e)
             return
         }
-        const decoder = new TextDecoder("utf-8")
-        return decoder.decode(bytes)
+        return text
+    }
+
+    static writeText(filepath: string, contents: string): boolean {
+        try {
+            this.ensureDir(filepath)
+            Deno.writeTextFileSync(filepath, contents)
+            return true
+        } catch (e) {
+            if (e instanceof Deno.errors.AlreadyExists) return true
+            if (e instanceof Deno.errors.PermissionDenied) return false
+            console.error('Unhandled error when attempting to write file', e)
+        }
+        return false
+    }
+
+    static remove(filepath: string): boolean {
+        try {
+            Deno.removeSync(filepath)
+        } catch (e) {
+            if (e instanceof Deno.errors.NotFound) return true
+            if (e instanceof Deno.errors.PermissionDenied) return false
+            if (e instanceof Deno.errors.IsADirectory) return false
+            console.error('Unhandled error when attempting to remove file', e)
+        }
+        return true
+    }
+
+    /**
+     *
+     * @param source
+     * @param destination
+     */
+    static copy(source: string, destination: string): boolean {
+        this.ensureDir(destination)
+        try {
+            Deno.copyFileSync(source, destination)
+        } catch(e) {
+            if(e instanceof Deno.errors.IsADirectory) return false
+            if(e instanceof Deno.errors.PermissionDenied) return false
+            console.error('Unhandled error when attempting to copy file', e)
+        }
+        return true
+    }
+
+    static exists(filepath: string): boolean {
+        try {
+            Deno.lstatSync(filepath)
+        } catch (e) {
+            if (e instanceof Deno.errors.NotFound) return false
+            console.error('Unhandled error when checking if file exists', e)
+            return false
+        }
+        return true
     }
 }
